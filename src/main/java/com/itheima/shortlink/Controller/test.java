12e4bde8ed8200aa9fe5d6210f72e4efd4daa289
FanBingQiang <3581505260@qq.com>
2026-03-01 13:57:22 +0800

    init

---
+++ src/main/java/com/itheima/shortlink/Controller/test.java
@@ -0,0 +1,12 @@
+package com.itheima.shortlink.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello, Short Link!";
+    }
+}
